862311302438f0644eb76f3ee6d6509b69820dc3
Brandt Newton <brandtnewton@google.com>
2025-08-28 12:07:34 -0400

    removed comments

--- cassandra-bigtable-migration-tools/cassandra-bigtable-proxy/testing/compliance/cqlsh_support_test.go
+++ cassandra-bigtable-migration-tools/cassandra-bigtable-proxy/testing/compliance/cqlsh_support_test.go
@@ -11,9 +11,9 @@ func TestCqlshCrud(t *testing.T) {
 	t.Parallel()
 
 	_, err := executeCQLQuery(`INSERT INTO bigtabledevinstance.user_info (name, age, code) VALUES ('cqlsh_person', 80, 25)`)
-	require.NoError(t, err, "Failed to insert record")
+	require.NoError(t, err)
 
 	results, err := executeCQLQuery(`SELECT name, age, code FROM bigtabledevinstance.user_info where name='cqlsh_person' and age=80`)
-	require.NoError(t, err, "Failed to insert record")
+	require.NoError(t, err)
 	assert.Equal(t, []map[string]string{{"age": "80", "name": "cqlsh_person", "code": "25"}}, results)
 }

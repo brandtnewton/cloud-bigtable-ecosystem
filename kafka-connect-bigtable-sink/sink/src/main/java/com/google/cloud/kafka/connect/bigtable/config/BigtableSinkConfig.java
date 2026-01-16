3a6b2d9bdc73a41bb480f06d77ec49af13ca68eb
Brandt Newton <brandtnewton@google.com>
2026-01-16 09:24:50 -0500

    adding config

--- kafka-connect-bigtable-sink/sink/src/main/java/com/google/cloud/kafka/connect/bigtable/config/BigtableSinkConfig.java
+++ kafka-connect-bigtable-sink/sink/src/main/java/com/google/cloud/kafka/connect/bigtable/config/BigtableSinkConfig.java
@@ -46,6 +46,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.kafka.common.config.AbstractConfig;
@@ -79,6 +80,7 @@ public class BigtableSinkConfig extends AbstractConfig {
   public static final String ROW_KEY_SOURCE_CONFIG = "row.key.source";
   public static final String ROW_KEY_DEFINITION_CONFIG = "row.key.definition";
   public static final String ROW_KEY_DELIMITER_CONFIG = "row.key.delimiter";
+  public static final String EXPAND_ROOT_LEVEL_ARRAYS = "expand.root.level.arrays";
   public static final String AUTO_CREATE_TABLES_CONFIG = "auto.create.tables";
   public static final String AUTO_CREATE_COLUMN_FAMILIES_CONFIG = "auto.create.column.families";
   public static final String DEFAULT_COLUMN_FAMILY_CONFIG = "default.column.family";
@@ -369,6 +371,15 @@ public class BigtableSinkConfig extends AbstractConfig {
             "The delimiter used in concatenating Kafka key fields in the row key. If this"
                 + " configuration is empty or unspecified, the key fields will be concatenated"
                 + " together directly.")
+        .define(
+            EXPAND_ROOT_LEVEL_ARRAYS,
+            ConfigDef.Type.BOOLEAN,
+            false,
+            ConfigDef.Importance.LOW,
+            "Determines whether root level arrays should be expanded to a column family or serialized to " +
+                "a single column. If true, root level array fields will be mapped to a Bigtable column family where " +
+                "each element is stored in an individual column. If false, root level array fields will be serialized " +
+                "as a JSON string to a single column.")
         .define(
             AUTO_CREATE_TABLES_CONFIG,
             ConfigDef.Type.BOOLEAN,

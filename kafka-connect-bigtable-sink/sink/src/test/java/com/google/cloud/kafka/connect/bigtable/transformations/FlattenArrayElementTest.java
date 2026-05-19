64c8f27c3dbd0146da08f77758234a03e6523ec0
Brandt Newton <brandtnewton@google.com>
2026-05-19 13:12:47 -0400

    fix

--- kafka-connect-bigtable-sink/sink/src/test/java/com/google/cloud/kafka/connect/bigtable/transformations/FlattenArrayElementTest.java
+++ kafka-connect-bigtable-sink/sink/src/test/java/com/google/cloud/kafka/connect/bigtable/transformations/FlattenArrayElementTest.java
@@ -376,7 +376,7 @@ public class FlattenArrayElementTest {
                     .put("id", "PROD-789")
                     .put("quantity", 2));
 
-    List<Struct> productList = List.of(null, productElement2, productElement3);
+    List<Struct> productList = Arrays.asList(null, productElement2, productElement3);
 
     Struct productsWrapper = new Struct(schema.field("products").schema()).put("list", productList);
 
